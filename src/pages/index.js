459b31810169f0557bc1ba7caa593e6b163820bc
morganchri <cmorgan1351@gmail.com>
2023-10-12 21:17:16 -0400

    Commit for props

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 // import Profile from './qcomps/profile_mistake/profile_mistake.js'
-import Gallery from './components/gallery.js'
+import Gallery from './qcomps/gallery_props.js'
 import FirstComp from './qcomps/firstcomp.js'
 import TodoList from './qcomps/todos.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <FirstComp />
+        <Gallery />
     </div>
   )
 }
--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,54 +1,50 @@
+import Avatar from "../components/profile_props";
+
+function Profile(scientist, size) {
+  return (
+    <div>
+    <img
+      className="avatar"
+      src={'https://i.imgur.com/' + scientist.imageId + 's.jpg'}
+      alt={scientist.name}
+      width={size}
+      height={size}
+    />
+    <ul>
+      <li>
+        <b>Profession: </b>
+            {scientist.profession};
+          </li>
+          <li>
+            <b>Awards: {scientist.awardCount} </b>
+            ({scientist.awards})
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {scientist.discovered}
+          </li>
+        </ul>
+        </div>
+  );
+}
+
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
       <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
+        <Profile scientist={{name:"Maria Skłodowska-Curie", imageId:"szV5sdGs",
+        profession:"physicist and chemist", awardCount:"4", 
+        awards:"Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal",
+        discovered:"polonium (element)"}}
+        size={70}/>
       </section>
       <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
+      <Profile scientist={{name:"Katsuko Saruhashi", imageId:"YfeOqp2s",
+        profession:"geochemist", awardCount:"2", 
+        awards:"Miyake Prize for geochemistry, Tanaka Prize",
+        discovered:"a method for measuring carbon dioxide in seawater"}}
+        size={70}/>
       </section>
     </div>
   );
